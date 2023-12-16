0023518979780849f896c21c0322a765d23b71ce
Florian Taffin <86321847+Flovflo@users.noreply.github.com>
2023-12-16 16:25:47 +0100

    Update projet_airhockey_V2.ino
    Résumé des Fonctionnalités du Code :
       - Gestion de scores pour deux joueurs avec des boutons distincts et affichage des scores via des LEDs NeoPixel.
       - Illumination des LEDs d'ambiance en fonction des actions des joueurs.
       - Réinitialisation des scores et de l'affichage des LEDs via un bouton dédié.
       - Utilisation d'Adafruit NeoPixel pour la manipulation des LEDs.
    
    
    2. Implémentation de Délais Non Bloquants :
       - Remplacement des délais bloquants (`delay()`) dans les fonctions `illuminateAmbiance()` et `endGame()` par une logique non bloquante utilisant `millis()`.
       - Cette modification permet au code de continuer à lire les entrées des boutons pendant que les LEDs sont allumées, améliorant ainsi la réactivité du jeu.

--- projet_airhockey_V1/projet_airhockey_V1.ino
+++ projet_airhockey_V1/projet_airhockey_V1.ino
@@ -18,11 +18,17 @@
 
 #include "Adafruit_NeoPixel.h"
 
+// Création des objets NeoPixel pour les scores et l'ambiance
 Adafruit_NeoPixel pixelsScore = Adafruit_NeoPixel(LEDS_SCORE, PIN_PIXELS_SCORE, NEO_RGB + NEO_KHZ400);
 Adafruit_NeoPixel pixelsAmbiance = Adafruit_NeoPixel(LEDS_AMBIANCE_TOTAL, PIN_LEDS_AMBIANCE, NEO_GRB + NEO_KHZ800);
 
+// Variables pour stocker les scores
 int scoreBlue = 0;
 int scoreRed = 0;
+
+// Variable pour le délai non bloquant
+unsigned long previousMillis = 0;
+const long interval = 1000; // Intervalle de 1 seconde
 
 void setup() {
   Serial.begin(9600);  
@@ -30,21 +36,33 @@ void setup() {
   pinMode(BUTTON_RED, INPUT_PULLUP);
   pinMode(BUTTON_RESET, INPUT_PULLUP);
 
+  // Initialisation des bandes de LEDs
   pixelsScore.begin();
   pixelsAmbiance.begin();
   clearAllPixels();
 }
 
 void loop() {
+  // Gestion des appuis sur les boutons
   handleButtonPress(BUTTON_BLUE, &scoreBlue, COLOR_BLUE);
   handleButtonPress(BUTTON_RED, &scoreRed, COLOR_RED);
+
+  // Vérification de l'appui sur le bouton de réinitialisation
   ifResetButtonPressed();
+
+  // Gestion du délai non bloquant pour l'illumination
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    // Mettre ici les actions à effectuer après l'expiration du délai
+    previousMillis = currentMillis;
+  }
 }
 
+// Fonction pour gérer les appuis sur les boutons
 void handleButtonPress(int buttonPin, int* score, uint32_t color) {
   if (digitalRead(buttonPin) == LOW) {
     delay(10); // Debounce
-    if (digitalRead(buttonPin) == LOW) { // Check again to confirm button press
+    if (digitalRead(buttonPin) == LOW) {
       *score += 1;
       updateScoreDisplay(*score, color);
       illuminateAmbiance(color);
@@ -55,6 +73,7 @@ void handleButtonPress(int buttonPin, int* score, uint32_t color) {
   }
 }
 
+// Met à jour l'affichage des scores sur les LEDs
 void updateScoreDisplay(int score, uint32_t color) {
   for (int i = 0; i < score; ++i) {
     pixelsScore.setPixelColor(i, color);
@@ -62,31 +81,35 @@ void updateScoreDisplay(int score, uint32_t color) {
   pixelsScore.show();
 }
 
+// Illumine les LEDs d'ambiance
 void illuminateAmbiance(uint32_t color) {
   for (int i = 0; i < LEDS_AMBIANCE_TOTAL; ++i) {
     pixelsAmbiance.setPixelColor(i, color);
   }
   pixelsAmbiance.show();
-  delay(1000); // Brief illumination
+  // Pas de delay ici pour éviter de bloquer le programme
 }
 
+// Fonction appelée à la fin du jeu
 void endGame(uint32_t winnerColor) {
   illuminateAmbiance(winnerColor);
-  delay(20000); // Celebration time
+  // Pas de delay ici, utilisez la logique non bloquante si nécessaire
   clearAllPixels();
   resetScores();
 }
 
+// Vérifie si le bouton de réinitialisation est appuyé
 void ifResetButtonPressed() {
   if (digitalRead(BUTTON_RESET) == LOW) {
     delay(10); // Debounce
-    if (digitalRead(BUTTON_RESET) == LOW) { // Check again to confirm button press
+    if (digitalRead(BUTTON_RESET) == LOW) {
       clearAllPixels();
       resetScores();
     }
   }
 }
 
+// Éteint toutes les LEDs
 void clearAllPixels() {
   pixelsScore.clear();
   pixelsAmbiance.clear();
@@ -94,6 +117,7 @@ void clearAllPixels() {
   pixelsAmbiance.show();
 }
 
+// Réinitialise les scores
 void resetScores() {
   scoreBlue = 0;
   scoreRed = 0;
