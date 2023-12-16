e4792cb70ec1f2c03dd4c2fc70531a1e2667e947
Florian Taffin <86321847+Flovflo@users.noreply.github.com>
2023-12-16 15:52:56 +0100

    Update projet_airhockey_V2.ino

--- projet_airhockey_V1/projet_airhockey_V1.ino
+++ projet_airhockey_V1/projet_airhockey_V1.ino
@@ -11,314 +11,90 @@
 #define LEDS_AMBIANCE_BLUE 50
 #define LEDS_AMBIANCE_RED 50
 
+// Définition des couleurs
+#define COLOR_BLUE pixels.Color(0, 0, 255)
+#define COLOR_RED pixels.Color(255, 0, 0)
+#define COLOR_OFF pixels.Color(0, 0, 0)
+
 #include "Adafruit_NeoPixel.h"
 
-//setup led score
 Adafruit_NeoPixel pixelsScore = Adafruit_NeoPixel(LEDS_SCORE, PIN_PIXELS_SCORE, NEO_RGB + NEO_KHZ400);
-//setup led ambiance
 Adafruit_NeoPixel pixelsAmbiance = Adafruit_NeoPixel(LEDS_AMBIANCE_TOTAL, PIN_LEDS_AMBIANCE, NEO_GRB + NEO_KHZ800);
 
-//variable globale
-int NB_BUT=0;
-int SCORE_BLUE=0;
-int SCORE_RED=0;
+int scoreBlue = 0;
+int scoreRed = 0;
 
-void setup() 
-{
-Serial.begin(9600);  
-//definir les bouttons en entrée et pullup
-pinMode(BUTTON_BLUE, INPUT_PULLUP);
-pinMode(BUTTON_RED, INPUT_PULLUP);
-pinMode(BUTTON_RESET, INPUT_PULLUP);
+void setup() {
+  Serial.begin(9600);  
+  pinMode(BUTTON_BLUE, INPUT_PULLUP);
+  pinMode(BUTTON_RED, INPUT_PULLUP);
+  pinMode(BUTTON_RESET, INPUT_PULLUP);
 
-  
-pixelsScore.begin();
-pixelsAmbiance.begin();
+  pixelsScore.begin();
+  pixelsAmbiance.begin();
+  clearAllPixels();
+}
 
-  // Éteindre tous les pixels
+void loop() {
+  handleButtonPress(BUTTON_BLUE, &scoreBlue, COLOR_BLUE);
+  handleButtonPress(BUTTON_RED, &scoreRed, COLOR_RED);
+  ifResetButtonPressed();
+}
+
+void handleButtonPress(int buttonPin, int* score, uint32_t color) {
+  if (digitalRead(buttonPin) == LOW) {
+    delay(10); // Debounce
+    if (digitalRead(buttonPin) == LOW) { // Check again to confirm button press
+      *score += 1;
+      updateScoreDisplay(*score, color);
+      illuminateAmbiance(color);
+      if (*score >= LEDS_SCORE) {
+        endGame(color);
+      }
+    }
+  }
+}
+
+void updateScoreDisplay(int score, uint32_t color) {
+  for (int i = 0; i < score; ++i) {
+    pixelsScore.setPixelColor(i, color);
+  }
+  pixelsScore.show();
+}
+
+void illuminateAmbiance(uint32_t color) {
+  for (int i = 0; i < LEDS_AMBIANCE_TOTAL; ++i) {
+    pixelsAmbiance.setPixelColor(i, color);
+  }
+  pixelsAmbiance.show();
+  delay(1000); // Brief illumination
+}
+
+void endGame(uint32_t winnerColor) {
+  illuminateAmbiance(winnerColor);
+  delay(20000); // Celebration time
+  clearAllPixels();
+  resetScores();
+}
+
+void ifResetButtonPressed() {
+  if (digitalRead(BUTTON_RESET) == LOW) {
+    delay(10); // Debounce
+    if (digitalRead(BUTTON_RESET) == LOW) { // Check again to confirm button press
+      clearAllPixels();
+      resetScores();
+    }
+  }
+}
+
+void clearAllPixels() {
   pixelsScore.clear();
   pixelsAmbiance.clear();
   pixelsScore.show();
   pixelsAmbiance.show();
 }
 
-void loop() 
-  {  
-    // Exécuter la fonction JOUEUR_BLUE
-    JOUEUR_BLUE();
-    // Exécuter la fonction JOUEUR_RED
-    JOUEUR_RED();
-    // Exécuter la fonction du boutton reset
-    IF_RESET_BOUTTON();
-
-  }
-
-void IF_RESET_BOUTTON() 
-  {
-    if(digitalRead(BUTTON_RESET)==LOW)
-          {
-            // Éteindre tous les pixels
-              pixelsScore.clear();
-              pixelsAmbiance.clear();
-              pixelsScore.show();
-              pixelsAmbiance.show();
-
-              NB_BUT=0;
-              SCORE_BLUE=0;
-              SCORE_RED=0;
-          }
-  }
-
-void RESET() 
-  {
- // Éteindre tous les pixels
-  pixelsScore.clear();
-  pixelsAmbiance.clear();
-  pixelsScore.show();
-  pixelsAmbiance.show();
-
-  NB_BUT=0;
-  SCORE_BLUE=0;
-  SCORE_RED=0;
-
-  }
-
-void ALLUMER_AMBIANCE_RED() 
-  {
-    //LEDS_AMBIANCE_TOTAL 
-    for (int i = 0; i < LEDS_AMBIANCE_TOTAL; i++) {
-      pixelsAmbiance.setPixelColor(i, 255, 0, 0); //blue
-    }
-    // Mettre à jour les pixels
-    pixelsAmbiance.show();
-    delay(1000);
-    for (int i = 0; i < LEDS_AMBIANCE_TOTAL; i++) {
-      pixelsAmbiance.setPixelColor(i, 255, 0, 0); //blue
-    }
-    // Mettre à jour les pixels
-    pixelsAmbiance.show();
-    delay(1000);
-        for (int i = 0; i < LEDS_AMBIANCE_TOTAL; i++) {
-      pixelsAmbiance.setPixelColor(i, 255, 0, 0); //blue
-    }
-    // Mettre à jour les pixels
-    pixelsAmbiance.show();
-    delay(3000);
-    ALLUMER_AMBIANCE_NORMAL();
-  }
-
-void ALLUMER_AMBIANCE_BLUE() 
-  {
-    //LEDS_AMBIANCE_TOTAL 
-    for (int i = 0; i < LEDS_AMBIANCE_TOTAL; i++) {
-      pixelsAmbiance.setPixelColor(i,  0, 0, 255); //blue
-    }
-    // Mettre à jour les pixels
-    pixelsAmbiance.show();
-    delay(1000);
-    for (int i = 0; i < LEDS_AMBIANCE_TOTAL; i++) {
-      pixelsAmbiance.setPixelColor(i,  0, 0, 0); //blue
-    }
-    // Mettre à jour les pixels
-    pixelsAmbiance.show();
-    delay(1000);
-        for (int i = 0; i < LEDS_AMBIANCE_TOTAL; i++) {
-      pixelsAmbiance.setPixelColor(i,  0, 0, 255); //blue
-    }
-    // Mettre à jour les pixels
-    pixelsAmbiance.show();
-    delay(3000);
-    ALLUMER_AMBIANCE_NORMAL();
-  }
-
-void ALLUMER_AMBIANCE_NORMAL() {
-  // Allumer LEDS_AMBIANCE_RED  pixels en rouge
-  for (int i = 0; i < LEDS_AMBIANCE_RED; i++) {
-    pixelsAmbiance.setPixelColor(i, 255, 0, 0);
-  }
-  // Allumer LEDS_AMBIANCE_BLUE derniers pixels en bleu
-  for (int i = LEDS_AMBIANCE_BLUE; i < 100; i++) {
-    pixelsAmbiance.setPixelColor(i, 0, 0, 255);
-  }
-  // Mettre à jour les pixels
-  pixelsAmbiance.show();
+void resetScores() {
+  scoreBlue = 0;
+  scoreRed = 0;
 }
-void JOUEUR_BLUE() 
-{
-  if(digitalRead(BUTTON_BLUE)==LOW)
-          {
-          SCORE_BLUE=SCORE_BLUE+1;
-          NB_BUT=NB_BUT+1;
-                Serial.println(NB_BUT);
-                Serial.println(SCORE_BLUE);
-            if (NB_BUT<7)
-             {
-                switch (SCORE_BLUE)
-                   {
-                      case 1: //Si SCORE_BLUE vaut 1
-                      pixelsScore.setPixelColor(1, 0, 0, 255); //Bleu
-                      pixelsScore.show(); //met à jour le bandeau de pixels en utilisant la fonction show
-                      ALLUMER_AMBIANCE_BLUE();
-                      delay(1000);//attente de 1s
-                      break; 
-                  
-                      case 2: 
-                      pixelsScore.setPixelColor(2, 0, 0, 255); 
-                      pixelsScore.show(); 
-                      ALLUMER_AMBIANCE_BLUE();
-                      delay(1000);
-                      break;
-                  
-                      case 3: 
-                      pixelsScore.setPixelColor(3, 0, 0, 255); 
-                      pixelsScore.show(); 
-                      ALLUMER_AMBIANCE_BLUE();
-                      delay(1000);
-                      break;  
-        
-                      case 4: 
-                      pixelsScore.setPixelColor(4, 0, 0, 255); 
-                      pixelsScore.show(); 
-                      ALLUMER_AMBIANCE_BLUE();
-                      delay(1000);
-                      break; 
-        
-                      case 5: 
-                      pixelsScore.setPixelColor(5, 0, 0, 255); 
-                      pixelsScore.show();
-                      ALLUMER_AMBIANCE_BLUE();
-                      delay(1000);
-                      break; 
-        
-                      case 6: 
-                      pixelsScore.setPixelColor(6, 0, 0, 255); 
-                      pixelsScore.show(); 
-                      ALLUMER_AMBIANCE_BLUE();
-                      delay(1000);
-                      break;
-        
-                      case 7: 
-                      pixelsScore.setPixelColor(7, 0, 0, 255); 
-                      pixelsScore.show();
-                      ALLUMER_AMBIANCE_BLUE(); 
-                      delay(1000);
-                      break;
-                    }
-
-          } 
-          else 
-          {
-            if ( SCORE_BLUE > SCORE_RED) { // si SCORE_BLUE est plus grand que SCORE_RED
-                          for (int i = 0; i < LEDS_AMBIANCE_TOTAL; i++) {
-                            pixelsAmbiance.setPixelColor(i,  0, 0, 255); //blue
-                          }
-                          // Mettre à jour les pixels
-                          pixelsAmbiance.show();
-                          delay(20000);
-                        }
-                        // Sinon
-                        else {
-                          for (int i = 0; i < LEDS_AMBIANCE_TOTAL; i++) {
-                            pixelsAmbiance.setPixelColor(i,  255, 0, 0); //red
-                          }
-                          // Mettre à jour les pixels
-                          pixelsAmbiance.show();
-                          delay(20000);
-                        }
-                        RESET();
-          }
-                      
-                      
-        }
- }
-
- //la meme focntion pour le joueur rouge
-void JOUEUR_RED()
-{
-  if(digitalRead(BUTTON_RED)==LOW)
-          {
-          SCORE_RED=SCORE_RED+1;
-          NB_BUT=NB_BUT+1;
-                Serial.println(NB_BUT);
-                Serial.println(SCORE_RED);
-            if (NB_BUT<7)
-             {
-                switch (SCORE_RED)
-                   {
-                      case 1: //Si SCORE_RED vaut 1
-                      pixelsScore.setPixelColor(1, 255, 0, 0); //Rouge
-                      pixelsScore.show(); //met à jour le bandeau de pixels en utilisant la fonction show
-                      ALLUMER_AMBIANCE_RED();
-                      delay(1000);//attente de 1s
-                      break; 
-                  
-                      case 2: 
-                      pixelsScore.setPixelColor(2, 255, 0, 0); 
-                      pixelsScore.show(); 
-                      ALLUMER_AMBIANCE_RED();
-                      delay(1000);
-                      break;
-                  
-                      case 3: 
-                      pixelsScore.setPixelColor(3, 255, 0, 0); 
-                      pixelsScore.show(); 
-                      ALLUMER_AMBIANCE_RED();
-                      delay(1000);
-                      break;  
-        
-                      case 4: 
-                      pixelsScore.setPixelColor(4, 255, 0, 0); 
-                      pixelsScore.show(); 
-                      ALLUMER_AMBIANCE_RED();
-                      delay(1000);
-                      break; 
-        
-                      case 5: 
-                      pixelsScore.setPixelColor(5, 255, 0, 0); 
-                      pixelsScore.show();
-                      ALLUMER_AMBIANCE_RED();
-                      delay(1000);
-                      break; 
-        
-                      case 6: 
-                      pixelsScore.setPixelColor(6, 255, 0, 0); 
-                      pixelsScore.show(); 
-                      ALLUMER_AMBIANCE_RED();
-                      delay(1000);
-                      break;
-        
-                      case 7: 
-                      pixelsScore.setPixelColor(7, 255, 0, 0); 
-                      pixelsScore.show();
-                      ALLUMER_AMBIANCE_RED(); 
-                      delay(1000);
-                      break;
-                      
-                    }
-          } 
-          else 
-          {
-            if ( SCORE_RED > SCORE_BLUE) { // si SCORE_RED est plus grand que SCORE_BLUE
-                          for (int i = 0; i < LEDS_AMBIANCE_TOTAL; i++) {
-                            pixelsAmbiance.setPixelColor(i,  255, 0, 0); //red
-                          }
-                          // Mettre à jour les pixels
-                          pixelsAmbiance.show();
-                          delay(20000);
-                        }
-                        // Sinon
-                        else {
-                          for (int i = 0; i < LEDS_AMBIANCE_TOTAL; i++) {
-                            pixelsAmbiance.setPixelColor(i,  0, 0, 255); //blue
-                          }
-                          // Mettre à jour les pixels
-                          pixelsAmbiance.show();
-                          delay(20000);
-                        }
-                        RESET();
-          }
-
-        }
-}
-
-
